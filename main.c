5d0ca1a4fa73e5c50f4f088258f72cb9016433e4
Tismey <108024662+Tismey@users.noreply.github.com>
2024-01-09 14:58:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <assert.h>
 
+
 typedef struct grille
 {
     char *tab[6];
@@ -12,9 +13,9 @@ typedef struct grille
 
 grille *creerGrille();
 void printGrille(grille *g);
-char poserJeton(grille *g, char pos);
+void poserJeton(grille *g, char pos);
 void freeGrille(grille *g);
-uint8_t checkVictoireRec(grille* g, char side);
+uint8_t checkVictoireRec(grille* g, char side,int,int);
 uint8_t interCheckVictoireRec(grille* g,uint8_t i, uint8_t j, uint8_t depth,char side, int, int);
 char jouerCoup(grille *g);
 
@@ -58,7 +59,6 @@ char poserJeton(grille *g, char pos)
         --i;
     }
     g->tab[i][pos - 'A'] = pion[g->currP];
-    return pion[g->currP];
 }
 
 char jouerCoup(grille *g)
@@ -78,13 +78,12 @@ char jouerCoup(grille *g)
         goto chercherCoup;
     }
     
-    action = poserJeton(g, action);
+    poserJeton(g, action);
     return action;
 }
 
-uint8_t checkVictoireRec(grille* g, char side){
-    for(uint8_t i = 0; i < 6; i++){
-        for(uint8_t j = 0; j < 7; j++){
+uint8_t checkVictoireRec(grille* g, char side,int i ,int j){
+    
             if(interCheckVictoireRec(g,i,j,4,side,1,1)
             + interCheckVictoireRec(g,i,j,4,side,-1,-1) > 4){
                 return side;
@@ -102,8 +101,8 @@ uint8_t checkVictoireRec(grille* g, char side){
                 return side;
             }
 
-        }
-    }
+        
+    
     return 0;
 }
 
@@ -147,7 +146,14 @@ int main()
         g->currP = 1 - g->currP;
         action = jouerCoup(g);
         printGrille(g);
-        if(checkVictoireRec(g, action) != 0){
+        uint8_t coord = 5;
+        char pion[2] = {'X', 'O'};
+        while (g->tab[i][pos - 'A'] != ' ')
+        {
+            --coord;
+        }
+
+        if(checkVictoireRec(g,pion[g->currP] ,coord,pos - 'A') != 0){//FIXME!!! Blyat
             g->gameStatus = 1;
             printf("Bravo, le joueur %d a gagné\n Voulez-vous rejouer? [y/n] \n", g->currP + 1);
             scanf(" %c", &action);
